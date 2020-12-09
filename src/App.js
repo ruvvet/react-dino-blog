c9ba406a4fa5222e8e3fb13ea95c3487d58541a3
Jenny <finej01@gmail.com>
2020-12-09 12:24:29 -0500

    added input function for comment

--- src/App.js
+++ src/App.js
@@ -1,28 +1,66 @@
 import logo from './logo.svg';
 import './App.css';
-import Post from './Post'
-import Dino from './Dino'
+import Post from './Post';
+import Dino from './Dino';
+import React, { Component } from 'react';
 
-function App(props) {
+// function App(props) {
 
+//   return (
+//     <div className="App">
+//            <p>Title: {props.post.title}</p>
+//         <p>Author: {props.post.author}</p>
+//         <p>Body: {props.post.body.title}</p>
+//         <p>1st Comment: {props.post.comments[0]}</p>
 
-  return (
-    <div className="App">
-           <p>Title: {props.post.title}</p>
-        <p>Author: {props.post.author}</p>
-        <p>Body: {props.post.body.title}</p>
-        <p>1st Comment: {props.post.comments[0]}</p>
+// <Dino dino={props.post} />
 
+//       {/* <Post post={post} /> ---if i wanted to pass it from another file */}
+//       {/* imported module name, varName={what's being passed to it} */}
 
-<Dino dino={props.post} />
+//     </div>
+//   );
+// }
 
+class App extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      title: props.post.title,
+      author: props.post.author,
+      body: props.post.body,
+      comments: props.post.comments,
+      // we created new inputtext key here
+    };
+  }
 
-      {/* <Post post={post} /> ---if i wanted to pass it from another file */}
-      {/* imported module name, varName={what's being passed to it} */}
+  newComment = () => {
+    let newArr = [...this.state.comments, this.state.inputText]; // [ [comments], newcomment]
 
+    this.setState({ comments: newArr });
+  };
 
-    </div>
-  );
+  changeInput = (evt) => {
+    this.setState({ inputText: evt.target.value });
+  };
+
+  render() {
+    return (
+      <div>
+        <p>Title: {this.state.title}</p>
+        <p>Author: {this.state.author}</p>
+        <p>Body: {this.state.body}</p>
+        <p>Comments: {this.state.comments.join(',')}</p>
+
+        <input
+          type="text"
+          value={this.state.inputText}
+          onChange={this.changeInput}
+        ></input>
+        <button onClick={this.newComment}>Add Comment</button>
+      </div>
+    );
+  }
 }
 
 export default App;
